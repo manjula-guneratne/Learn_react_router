dae8ab45eb4fd0c3cd8b1962a9086a3ff9fbdbfd
Manjula <mguneratne3@gmail.com>
2024-02-09 16:02:47 -0500

    Not found added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Home } from "./pages/Home"
 import { BookList } from "./pages/BookList"
 import { Book } from "./pages/Book"
 import { NewBook } from "./pages/NewBook"
+import {NotFound} from "./pages/NotFound"
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/books" element={<BookList />} />
         <Route path="/books/:id" element={<Book />} />
         <Route path="/books/new" element={<NewBook />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
