5584d0162b426e626b8add2eecb2d84aa22520ec
Manjula <mguneratne3@gmail.com>
2024-02-09 16:19:40 -0500

    path layout added

--- src/pages/BookList.js
+++ src/pages/BookList.js
@@ -1,14 +1,3 @@
-import { Link } from "react-router-dom";
-
 export function BookList() {
-    return (
-        <>
-            <h1>Book</h1>
-            <Link to="/books/1">Book 1</Link>
-            <br />
-            <Link to="/books/2">Book 2</Link>
-            <br />
-            <Link to="/books/new">New Book</Link>
-        </>
-    );
+  return <h1>Book</h1>;
 }
