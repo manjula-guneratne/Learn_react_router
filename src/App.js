718e086b277d981423ee8ba9447917f02aafa8aa
Manjula <mguneratne3@gmail.com>
2024-02-09 16:33:23 -0500

    Outlet added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import { Link, Route, Routes } from "react-router-dom"
-import { Home } from "./pages/Home"
-import { BookList } from "./pages/BookList"
-import { Book } from "./pages/Book"
-import { NewBook } from "./pages/NewBook"
-import {NotFound} from "./pages/NotFound"
-import { BookLayout } from "./BookLayout"
+import { Link, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { BookList } from "./pages/BookList";
+import { Book } from "./pages/Book";
+import { NewBook } from "./pages/NewBook";
+import { NotFound } from "./pages/NotFound";
+import { BookLayout } from "./BookLayout";
 
 function App() {
   return (
     <>
+      <Routes>
+        <Route path="/books" element={<h1>Extra Content</h1>} />
+      </Routes>
       <nav>
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/books">Book</Link></li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/books">Book</Link>
+          </li>
         </ul>
       </nav>
       <Routes>
--- src/pages/Book.js
+++ src/pages/Book.js
@@ -1,6 +1,11 @@
-import { useParams } from "react-router-dom"
+import { useOutletContext, useParams } from "react-router-dom";
 
 export function Book() {
-    const { id } = useParams()
-    return <h1>Book {id}</h1>
+  const { id } = useParams();
+  const obj = useOutletContext();
+  return (
+    <h1>
+      Book {id} {obj.hello}
+    </h1>
+  );
 }
