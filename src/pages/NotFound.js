bab3d83528299dca0465934af376252136c14aed
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2024-02-10 00:04:55 -0500

    SetTimeout, useNav and useEffect added

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,3 +1,14 @@
-export function NotFound(){
-    return <h1>NotFound</h1>
+import { Navigate, useNavigate } from "react-router-dom"
+import { useEffect } from "react"
+
+export function NotFound() {
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        setTimeout(() => {
+            navigate("/")
+        }, 1000)
+    }, [])
+
+    return <h1>Not Found</h1>
 }
